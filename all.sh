612fa7ce2b5f8fb46c67d2ed0472325e5496766c
gabs <110247388+GabsEdits@users.noreply.github.com>
2022-12-24 16:14:49 +0200

    Added theme scripts

--- all.sh
+++ all.sh
@@ -30,11 +30,20 @@ rpm -i code-1.74.2-1671533504.el7.x86_64.rpm
 # Clear Cache
 sudo dnf clean dbcache OR dnf clean all
 
-# To do manual:
-# sudo nano /etc/dnf/dnf.conf
-# ------------
+# Theme
+dnf copr enable nickavem/adw-gtk3
+dnf install adw-gtk3
+gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark' && gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+cd /home/gaba
+wget https://github.com/ful1e5/apple_cursor/releases/download/v2.0.0/macOS-Monterey.tar.gz
+tar -xvf macOS-Monterey.tar.gz
+mkdir ./icons
+mv macOS-* ~/.icons/
+
+# DNF Config
+sudo nano /etc/dnf/dnf.conf
 # fastestmirror=True
 # max_parallel_downloads=10
 # defaultyes=True
 # keepcache=True
-# -----------
+# --------
