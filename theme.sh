11676dd98d99080331b1bdde89d418b7981d7e27
gabs <110247388+GabsEdits@users.noreply.github.com>
2023-01-08 20:22:47 +0200

    Update Cursor

--- theme.sh
+++ theme.sh
@@ -8,8 +8,5 @@ fi
 dnf copr enable nickavem/adw-gtk3
 dnf install adw-gtk3
 gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark' && gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-cd /home/gaba
-wget https://github.com/ful1e5/apple_cursor/releases/download/v2.0.0/macOS-Monterey.tar.gz
-tar -xvf macOS-Monterey.tar.gz
-mkdir ./icons
-mv macOS-* ~/.icons/
+sudo dnf copr enable peterwu/rendezvous
+sudo dnf install bibata-cursor-themes
