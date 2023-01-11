b59f579552e3d1f7aa142bf57756879c112e8528
GabsEdits <gaba.edits@skiff.com>
2023-01-11 22:06:22 +0200

    Remove sudo

---
+++ test.sh
@@ -0,0 +1,7 @@
+if [[ $EUID -ne 0 ]]; then
+  echo "need root" 2>&1
+  exit 1
+fi
+
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
