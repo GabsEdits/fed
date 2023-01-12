13cf9954932fb065e8f16070fa5377be45213ec4
gabs <110247388+GabsEdits@users.noreply.github.com>
2023-01-12 18:01:18 +0200

    Add Cursor Fix

--- flat.sh
+++ flat.sh
@@ -1,1 +1,3 @@
- flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak --user override --filesystem=/home/$USER/.icons/:ro
+flatpak --user override --filesystem=/usr/share/icons/:ro
