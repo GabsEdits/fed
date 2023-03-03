2b63f37221fb3de77a068731fa5bfd515068ea94
gabs <110247388+GabsEdits@users.noreply.github.com>
2023-03-03 22:11:57 +0200

    Looks nicer

--- second.sh
+++ second.sh
@@ -7,30 +7,28 @@ fi
 echo "Run first.sh to have a faster exp. with dnf."
 
 # Enable flathub repo + fix cursor in flatpak apps
+echo "Adding Flathub"
  flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
  flatpak --user override --filesystem=/home/$USER/.icons/:ro
  flatpak --user override --filesystem=/usr/share/icons/:ro
  
 # Enable RPM Fusion
+echo "Adding RPMFusion"
 dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 
 # Media Codecs
+echo "Adding Multimedia Codecs"
 dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
 dnf groupupdate sound-and-video -y
 
 # Apps
+echo "Installing apps"
 dnf install discord -y
 wget https://download.cdn.viber.com/desktop/Linux/viber.rpm && rpm -i viber.rpm
 flatpak install flathub com.jetbrains.IntelliJ-IDEA-Community -y
 
 # Theme
+echo "Setting up adw-gtk3 & dark sytle"
 dnf copr enable nickavem/adw-gtk3 -y
 dnf install adw-gtk3 -y
 gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark' && gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-
-# DNF Config
-echo "To make dnf faster do nano /etc/dnf/dnf.conf then add the following:
-fastestmirror=True
-max_parallel_downloads=10
-defaultyes=True
-keepcache=True"
