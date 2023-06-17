7a215276daed5f19d90098ba7c4e888d18a5a819
Gabriel C <me@gabs.eu.org>
2023-06-17 19:17:29 +0300

    Fix adw-qt

--- script.sh
+++ script.sh
@@ -63,7 +63,7 @@ fi
       ;;
     a)
       dnf remove firefox -y
-      dnf remove adwaita-qt -y
+      dnf remove adwaita-qt5 -y
       wget https://download.cdn.viber.com/desktop/Linux/viber.rpm && rpm -i viber.rpm
       rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg && printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscodium.repo
       dnf install codium
